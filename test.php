f9ad102b99a5967969adb475229e8a973e1a087b
sam-korsh <skorshenkos@gmail.com>
2024-08-06 18:15:03 +0300

    changed data for the database and changed namespaces

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <?php
 
-use FpDbTest\Database;
-use FpDbTest\DatabaseTest;
+use DevTest\Database;
+use DevTest\DatabaseTest;
 
 spl_autoload_register(function ($class) {
     $a = array_slice(explode('\\', $class), 1);
@@ -12,7 +12,7 @@ spl_autoload_register(function ($class) {
     require_once $filename;
 });
 
-$mysqli = @new mysqli('localhost', 'root', 'password', 'database', 3306);
+$mysqli = @new mysqli('127.0.0.1', 'root', 'root', 'dev_test', 8101);
 if ($mysqli->connect_errno) {
     throw new Exception($mysqli->connect_error);
 }
